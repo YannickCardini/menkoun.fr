992ef7c7b1ad6d6cd99a65ee43de13f85a65a513
loubard <yannick.cardini@gmx.com>
2022-07-16 12:27:29 +0700

    Ajoute une semaine & fix error handling :+1:

--- html/poster-une-annonce.php
+++ html/poster-une-annonce.php
@@ -4,6 +4,7 @@ $user = "loubard";
 $password = "password";
 $database = "mainecoon";
 $table = "donation";
+$servername = "localhost";
 
 //sql value from form
 $catname = str_replace(['"',"'"], "\'", $_POST['catname']);
@@ -12,6 +13,10 @@ $region = $_POST['region'];
 $email = str_replace(['"',"'"], "\'", $_POST['email']);;
 $phone = $_POST['phone'];
 $date = date('Y-m-d H:i:s');
+$date = date('Y-m-d H:i:s', strtotime($date . "+1 week"));
+
+$sql = "INSERT INTO mainecoon.donation (catname, descri, region, img, phone, email, dateposted)
+VALUES ('$catname', '$descri', '$region','$target_file','$phone','$email','$date')";
 
 //form photo
 $target_dir = "/uploads/";
@@ -20,21 +25,51 @@ $uploadOk = 1;
 $error_txt = "Une erreur s'est produite";
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-$error_page = $_SERVER["DOCUMENT_ROOT"] . "/error.html";
+$error_page = "./error.html";
 
-// Insert into mainecoon.donation
-try {
-  $query = "INSERT INTO mainecoon.donation (catname, descri, region, img, phone, email, dateposted)
-  VALUES ('$catname', '$descri', '$region','$target_file','$phone','$email','$date')";
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  $db->query($query);
-} catch (PDOException $e) {
+$con=mysqli_connect($servername,$user,$password,$database);
+
+if (mysqli_connect_errno()) {
   $pagecontents = file_get_contents($error_page);
-  echo str_replace("tittle", $e->getMessage(), $pagecontents);
-  die();
+  echo str_replace("tittle", mysqli_connect_error(), $pagecontents);
+  exit();
 }
 
+// Perform a query, check for error
+if (!mysqli_query($con,$sql)) {
+  $pagecontents = file_get_contents($error_page);
+  echo str_replace("tittle", mysqli_error($con), $pagecontents);
+  exit();
+}
+
+mysqli_close($con);
+
+
+// // Insert into mainecoon.donation
+// try {
+//   $query = "INSERT INTO mainecoon.donation (catname, descri, region, img, phone, email, dateposted)
+//   VALUES ('$catname', '$descri', '$region','$target_file','$phone','$email','$date')";
+//   $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+//   $db->query($query);
+//   echo $query;
+
+// } catch (PDOException $e) {
+//   echo $e;
+//   $pagecontents = file_get_contents($error_page);
+//   echo str_replace("tittle", $e->getMessage(), $pagecontents);
+//   die();
+// }
+
 //Upload the photo to /var/www/menkoun.fr/html/uploads/
+
+// Check if photo has been uploaded
+if (isset ( $_FILES ['photo'] )) {
+  if (!$_FILES ["photo"] ["error"] == UPLOAD_ERR_OK) {
+      include("success.html");
+  }
+} else {
+  include("success.html");
+}
 
 // Check if image file is a actual image or fake image
 if (isset($_POST["submit"])) {
@@ -45,6 +80,9 @@ if (isset($_POST["submit"])) {
     $error_txt = "Le fichier n'est pas une vrai image";
     $uploadOk = 0;
   }
+}
+else{
+  include("success.html");
 }
 
 // Check if file already exists
@@ -74,7 +112,7 @@ if ($uploadOk == 0) {
   // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["photo"]["tmp_name"], $_SERVER["DOCUMENT_ROOT"] . $target_file)) {
-    include($_SERVER["DOCUMENT_ROOT"] . "/success.html");
+    include("success.html");
   } else {
     $pagecontents = file_get_contents($error_page);
     echo str_replace("tittle", "Une erreur lors du téléchargement du fichier", $pagecontents);
